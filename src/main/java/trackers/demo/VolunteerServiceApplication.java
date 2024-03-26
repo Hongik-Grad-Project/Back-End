b29fec64692f832cae899ca30941b012d44cf7da
Yoon-Jemin <yjm1800@naver.com>
2024-03-26 15:38:01 +0900

    first commit

---
+++ src/main/java/trackers/demo/VolunteerServiceApplication.java
@@ -0,0 +1,13 @@
+package trackers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trackers/demo/VolunteerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package trackers.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
