07924705c1406b726414ffb61288e850a1bef51d
Yoon-Jemin <yjm1800@naver.com>
2024-04-16 00:19:51 +0900

    네이버, 구글 로근인 완료

--- src/main/java/trackers/demo/global/auth/jwt/JWTFilter.java
+++ src/main/java/trackers/demo/global/auth/jwt/JWTFilter.java
@@ -1,5 +1,6 @@
 package trackers.demo.global.auth.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -15,6 +16,7 @@ import trackers.demo.global.auth.oauth.dto.CustomOAuth2User;
 import trackers.demo.global.auth.oauth.dto.UserDTO;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {   // 필터는 한 번만 요청되면 됨
@@ -23,42 +25,46 @@ public class JWTFilter extends OncePerRequestFilter {   // 필터는 한 번만 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        /* cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음 */
-        String authorization = null;
-        Cookie[] cookies = request.getCookies();
-        for(Cookie cookie : cookies){
+        // 헤더에서 access 키에 담긴 토큰을 꺼냄
+        String accessToken = request.getHeader("access");
 
-            System.out.println(cookie.getName());
-            if(cookie.getName().equals("Authorization")){
-                authorization = cookie.getValue();
-            }
-        }
-
-        // Authorization 헤더 검증
-        if (authorization == null){
-            System.out.println("token null");
-            filterChain.doFilter(request,response);
-
+        // 토큰이 없다면 다음 필터로 넘김
+        if(accessToken == null){
+            filterChain.doFilter(request, response);
             return;
         }
 
-        // 토큰
-        String token = authorization;
+        // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
 
-        //토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(token)) {
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
 
-            System.out.println("token expired");
-            filterChain.doFilter(request, response);
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
 
-            //조건이 해당되면 메소드 종료 (필수)
+        // 토큰이 access인지 확인 (발급시 페이로드에 명시), 다음 필터로 넘기지 않음
+        String category = jwtUtil.getCategory(accessToken);
+
+        if (!category.equals("access")) {
+
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access token");
+
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
         //토큰에서 authKey와 role 획득
-        String authKey = jwtUtil.getAuthkey(token);
-        String role = jwtUtil.getRole(token);
-
+        String authKey = jwtUtil.getAuthkey(accessToken);
+        String role = jwtUtil.getRole(accessToken);
 
         //UserDTO를 생성하여 빌드
         UserDTO userDTO = UserDTO.builder()
@@ -72,7 +78,7 @@ public class JWTFilter extends OncePerRequestFilter {   // 필터는 한 번만 
         //스프링 시큐리티 인증 토큰 생성
         Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2User, null, customOAuth2User.getAuthorities());
 
-        //세션에 사용자 등록
+        //세션에 사용자 등록 -> 요청에 대해서 로그인된 상태로 변경
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
--- src/main/java/trackers/demo/global/auth/oauth/CustomSuccessHandler.java
+++ src/main/java/trackers/demo/global/auth/oauth/CustomSuccessHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -25,7 +26,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        //OAuth2User
+        //OAuth2User(유저 정보) authKey와 role을 가져옴
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
 
         String authKey = customUserDetails.getAuthKey();
@@ -35,16 +36,20 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-        String token = jwtUtil.createJwt(authKey, role, 60*60*60L);
+        // 다중 토큰 발급
+        String access = jwtUtil.createJwt("access", authKey, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", authKey, role, 86400000L);
 
-        response.addCookie(createCookie("Authorization", token));
-        response.sendRedirect("http://localhost:3000/");
+        // 응답 설정
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
     }
 
     private Cookie createCookie(String key, String value) {
 
         Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(60*60*60);
+        cookie.setMaxAge(24*60*60);
         //cookie.setSecure(true);   // 오직 HTTPS 통신에서만 쿠키 사용
         cookie.setPath("/");
         cookie.setHttpOnly(true);   // javascript가 쿠키를 가져가지 못함
