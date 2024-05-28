8b5f7f36a1b8ced9d17f92c90ca1f51228fff622
Yoon-Jemin <yjm1800@naver.com>
2024-05-28 16:07:48 +0900

    [FIX] socialLoginId 길이 수정

--- src/main/java/trackers/demo/member/domain/Member.java
+++ src/main/java/trackers/demo/member/domain/Member.java
@@ -23,7 +23,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 80)
     private String socialLoginId;
 
     @Column(nullable = false, length = 50)
