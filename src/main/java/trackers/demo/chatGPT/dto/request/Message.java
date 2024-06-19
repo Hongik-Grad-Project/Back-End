1f8e1a84fa9803f7291f8d2c94b5233a9463a7e1
Yoon-Jemin <yjm1800@naver.com>
2024-06-19 21:05:55 +0900

    [fix] application-secret.yml 업데이트

--- src/main/java/trackers/demo/chatGPT/dto/request/Message.java
+++ src/main/java/trackers/demo/chatGPT/dto/request/Message.java
@@ -1,14 +1,14 @@
-//package trackers.demo.chatGPT.dto.request;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Message {
-//
-//    private String role;
-//    private String content;     // 프롬프트
-//}
+package trackers.demo.chatGPT.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String role;
+    private String content;     // 프롬프트
+}
--- src/main/java/trackers/demo/chatGPT/dto/response/ChatGPTResponse.java
+++ src/main/java/trackers/demo/chatGPT/dto/response/ChatGPTResponse.java
@@ -1,26 +1,26 @@
-//package trackers.demo.chatGPT.dto.response;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import trackers.demo.chatGPT.dto.request.Message;
-//
-//import java.util.List;
-//
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class ChatGPTResponse {
-//
-//    private List<Choice> choices;
-//
-//    @Data
-//    @AllArgsConstructor
-//    @NoArgsConstructor
-//    public static class Choice {
-//
-//        private int index;
-//        private Message message;
-//    }
-//}
+package trackers.demo.chatGPT.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import trackers.demo.chatGPT.dto.request.Message;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatGPTResponse {
+
+    private List<Choice> choices;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+
+        private int index;
+        private Message message;
+    }
+}
