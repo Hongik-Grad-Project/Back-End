e03e14d1e6e5cbe62abb73f74d4db1d8f77207fe
Yoon-Jemin <yjm1800@naver.com>
2024-08-22 17:08:02 +0900

    [fix] 이전 채팅을 반영하도록 설정

--- src/main/java/trackers/demo/chat/dto/request/ChatGPTRequest.java
+++ src/main/java/trackers/demo/chat/dto/request/ChatGPTRequest.java
@@ -2,13 +2,18 @@ package trackers.demo.chat.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import trackers.demo.chat.domain.type.ChatMessageRole;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static trackers.demo.chat.domain.type.ChatMessageRole.*;
+
 @Getter
 @NoArgsConstructor
 public class ChatGPTRequest {
+
+    private static final String AURORA_AI_ROLE = "너는 친절한 상담가야 답변은 짧고 질문 위주로 해줘";
 
     private String model;
     private List<ChatGPTMessage> messages;
@@ -16,6 +21,15 @@ public class ChatGPTRequest {
     public ChatGPTRequest(final String model, final String prompt){
         this.model = model;
         this.messages = new ArrayList<>();
-        this.messages.add(new ChatGPTMessage("user", prompt));
+        this.messages.add(new ChatGPTMessage(SYSTEM.value(), AURORA_AI_ROLE));
+        this.messages.add(new ChatGPTMessage(USER.value(), prompt));
+    }
+
+    public ChatGPTRequest(final String model, final List<ChatGPTMessage> messages, final String prompt){
+        this.model = model;
+        this.messages = new ArrayList<>();
+        this.messages.add(new ChatGPTMessage(SYSTEM.value(), AURORA_AI_ROLE));
+        this.messages.addAll(messages);
+        this.messages.add(new ChatGPTMessage(USER.value(), prompt));
     }
 }
